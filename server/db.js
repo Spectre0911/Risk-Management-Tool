3988eaad146c63014c5bb602c87c1a3eccbc8f27
sanjula hettiarachchige <sanjulahetts@outlook.com>
2023-02-08 13:23:39 +0000

    updated db password

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "san",
+  password: "root",
   host: "localhost",
   port: 5432,
   database: "riskmanager"
