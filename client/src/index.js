892932af424331716de2f3a8654a1f3d1ab1292d
JoshuaCastelino <67103454+JoshuaCastelino@users.noreply.github.com>
2023-02-17 18:07:50 +0000

    Fixed all react console errors

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
 
-import './index.css';
-import { Provider } from 'react-redux';
-import {store} from "./store";
-import App from './App';
+import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import App from "./App";
 
 // REDUX
-import {ProSidebarProvider} from 'react-pro-sidebar';
+import { ProSidebarProvider } from "react-pro-sidebar";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
-    <Provider store={store}>
-        <ProSidebarProvider>
-            <App />
-        </ProSidebarProvider>
-    </Provider>,
-document.getElementById('root'),
-
+root.render(
+  <Provider store={store}>
+    <ProSidebarProvider>
+      <App />
+    </ProSidebarProvider>
+  </Provider>,
+  document.getElementById("root")
 );
