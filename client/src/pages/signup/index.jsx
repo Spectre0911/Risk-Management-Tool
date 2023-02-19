c2866b554ace9e5cbd6ca4322ddb3af5b83d0bfc
JoshuaCastelino <67103454+JoshuaCastelino@users.noreply.github.com>
2023-02-19 19:00:46 +0000

    Updated signup form, working on logic

--- client/src/pages/signup/index.jsx
+++ client/src/pages/signup/index.jsx
@@ -1,22 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
-import 'bootstrap/dist/css/bootstrap.css';
-import '../login/index.css';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "../login/index.css";
+import { useState } from "react";
 
 const SignUpPage = () => {
-  const [fullName, setFullName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     if (password !== confirmPassword) {
-      setErrorMessage('Passwords do not match.');
+      setErrorMessage("Passwords do not match.");
       return;
     }
 
@@ -24,61 +26,86 @@ const SignUpPage = () => {
   };
 
   return (
-    <body className='waveBg'>
-    <div className="signup-container">
-      <form className="loginForm" onSubmit={handleSubmit}>
-        <h1 className='loginH1'>Sign Up</h1>
-        {errorMessage && <p className="error">{errorMessage}</p>}
-        <div className="loginForm-group">
-          <label class="caption" htmlFor="fullName">Full Name:</label>
-          <input
-            type="text"
-            id="fullName"
-            className="loginForm-control"
-            value={fullName}
-            onChange={(event) => setFullName(event.target.value)}
-            required
-          />
-        </div>
-        <div className="loginForm-group">
-          <label class="caption" htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            className="loginForm-control"
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            required
-          />
-        </div>
-        <div className="loginForm-group">
-          <label class="caption" htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            className="loginForm-control"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            required
-          />
-        </div>
-        <div className="loginForm-group">
-            <label class="caption" htmlFor="confirmPassword">Confirm Password:</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            className="loginForm-control"
-            value={confirmPassword}
-            onChange={(event) => setConfirmPassword(event.target.value)}
-            required
-          />
-        </div>
-        <button type="submit" className="submit-button">Sign Up</button>
-        <div>
-          <label class="signupLabel">Already got an account? <a href="/login">Sign In</a></label>
-        </div>
-      </form>
-    </div>
+    <body className="waveBg">
+      <div className="signup-container">
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <h1 className="loginH1">Sign Up</h1>
+          {errorMessage && <p className="error">{errorMessage}</p>}
+          <div className="loginForm-group">
+            <label class="caption" htmlFor="firstName">
+              First Name:
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              className="loginForm-control"
+              value={firstName}
+              onChange={(event) => setFirstName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="loginForm-group">
+            <label class="caption" htmlFor="lastName">
+              Last Name:
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              className="loginForm-control"
+              value={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+              required
+            />
+          </div>
+          <div className="loginForm-group">
+            <label class="caption" htmlFor="email">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="loginForm-control"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              required
+            />
+          </div>
+          <div className="loginForm-group">
+            <label class="caption" htmlFor="password">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="loginForm-control"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+          </div>
+          <div className="loginForm-group">
+            <label class="caption" htmlFor="confirmPassword">
+              Confirm Password:
+            </label>
+            <input
+              type="password"
+              id="confirmPassword"
+              className="loginForm-control"
+              value={confirmPassword}
+              onChange={(event) => setConfirmPassword(event.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Sign Up
+          </button>
+          <div>
+            <label class="signupLabel">
+              Already got an account? <a href="/login">Sign In</a>
+            </label>
+          </div>
+        </form>
+      </div>
     </body>
   );
 };
