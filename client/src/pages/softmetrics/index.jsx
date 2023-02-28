e52f5ba4491c2fca79ceaaff69c5e0380ac021bb
morgankippen <119123934+morgankippen@users.noreply.github.com>
2023-02-28 12:49:14 +0000

    soft metrics page

---
+++ client/src/pages/softmetrics/index.jsx
@@ -0,0 +1,151 @@
+import React, { Component, useState, } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import {BsFillExclamationTriangleFill} from 'react-icons/bs';
+import {BsFillChatSquareTextFill} from 'react-icons/bs';
+import { BiTask } from "react-icons/bi";
+import { AiFillWarning } from "react-icons/ai";
+import { useParams } from "react-router-dom";
+import { Scrollbars } from "react-custom-scrollbars";
+import "../projectDashboard/index.css";
+import "./index.css";
+import { height } from "@mui/system";
+import { Button } from "react-bootstrap";
+import {CgUserRemove} from 'react-icons/cg';
+import Modal from 'react-bootstrap/Modal';
+import {GrClose} from 'react-icons/gr';
+import MultiLineChart from "./MultiLineChart";
+import HalfDoughnutChart from "./HalfDoughnutChart";
+
+import {
+  Box,
+  TextField,
+  useMediaQuery,
+  Typography,
+  useTheme,
+} from "@mui/material";
+
+const SoftMetrics = () => {
+
+  return (
+    <div className="main">
+      <div className="grid">
+        <p className="projectTitleId">Soft Metric Hub</p>
+
+        <div className="infoBox2 projectTable feature">
+          <div className="metricTitle2">Features</div>
+          <div className="ganttContainer overviewContainer">
+            <MultiLineChart/>
+          </div>
+        </div>
+        
+        <div className="infoBox2">
+          <Scrollbars>
+            <div className="metricTitle2" style={{marginBottom:'20px'}}>Notifications</div>
+
+            <div className="notificationBox">
+              <div className="notificationIcon blueIcon">
+                <BsFillChatSquareTextFill />
+              </div>
+
+              <div className="NotificationText">
+                <p>New Feedback</p>
+              </div>
+
+              <div className="NotificationDescription">
+                <p>
+                  From: John Smith
+                </p>
+              </div>
+            </div>
+
+            <div className="notificationBox">
+              <div className="notificationIcon blueIcon">
+                <BsFillChatSquareTextFill />
+              </div>
+
+              <div className="NotificationText">
+                <p>New Feedback</p>
+              </div>
+
+              <div className="NotificationDescription">
+                <p>From: Jane Doe</p>
+              </div>
+            </div>
+            <div className="notificationBox">
+              <div className="notificationIcon blueIcon">
+                <BsFillChatSquareTextFill />
+              </div>
+
+              <div className="NotificationText">
+                <p>New Feedback</p>
+              </div>
+
+              <div className="NotificationDescription">
+                <p>From: Charis</p>
+              </div>
+            </div>
+
+            <div className="notificationBox">
+              <div className="notificationIcon">
+                <BsFillExclamationTriangleFill />
+              </div>
+
+              <div className="NotificationText">
+                <p>Risk Rising</p>
+              </div>
+
+              <div className="NotificationDescription">
+                <p>Communication risk level is higher than previous week</p>
+              </div>
+            </div>
+          </Scrollbars>
+        </div>
+      
+        <div className="infoBox project">
+        
+        <div className="metricTitle">Communication</div>
+        <div className="metricHalfDonutContainer">
+          <HalfDoughnutChart data1={3.5} label="Communication" />
+          <div className="donutText halfdonutRisk">
+            <p>3.5</p>
+          </div>
+        </div>    
+      </div>
+
+      <div className="infoBox project">
+        <div className="metricTitle">Effective Decisions</div>
+        <div className="metricHalfDonutContainer">
+          <HalfDoughnutChart data1={4.1} label="Effective Decisions" />
+          <div className="donutText halfdonutRisk">
+            <p>4.1</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="infoBox project">
+        <div className="metricTitle">Core Values</div>
+        <div className="metricHalfDonutContainer">
+          <HalfDoughnutChart data1={3.8} label="Core Values" />
+          <div className="donutText halfdonutRisk">
+            <p>3.8</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="infoBox project">
+        <div className="metricTitle">Confidence in Skillset</div>
+        <div className="metricHalfDonutContainer">
+          <HalfDoughnutChart data1={4.4} label="Confidence in Skillsets" />
+          <div className="donutText halfdonutRisk">
+            <p>4.4</p>
+          </div>
+        </div>
+      </div>
+
+      </div>     
+    </div>
+  );
+};
+
+export default SoftMetrics;
