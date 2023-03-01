5dd491d6e5e8e029596e0a536272cabe945de749
square <hannahkgutsche1@gmail.com>
2023-03-01 18:34:08 +0000

    Task-related queries

--- server/queries.sql
+++ server/queries.sql
@@ -15,3 +15,9 @@ select projectname, deadline, currentrisk from (select * from projects natural j
 
 -- Features
 select featurename, progress, starttime, endtime, currentrisk from features natural join userproject where userid = <userid here>;
+
+-- Total tasks
+select count(*) from (select * from tasks inner join features on featureid where projectid = <projectid here>);
+
+-- Tasks to be completed
+select count(*) from (select * from tasks inner join features on featureid where projectid = <projectid here> and not completed);
