1e97f55b561b1aa18e9fa7d0800a2de982b8d40c
square <hannahkgutsche1@gmail.com>
2023-03-02 17:19:14 +0000

    Added currentrisk attribute to projects table

--- server/queries.sql
+++ server/queries.sql
@@ -11,13 +11,13 @@ select count(*) from notifications where userid = <userid here> and not seen;
 -- TODO
 
 -- Project summary
-select projectname, firstname as managerfn, lastname as managerln, deadline from ((select projectid, projectname, deadline from projects natural join userproject where userid = <userid here>) natural join userproject where ismanager) natural join users;
+select projectname, firstname as managerfn, lastname as managerln, deadline, currentrisk from ((select projectid, projectname, deadline from projects natural join userproject where userid = <userid here>) as projectinfo natural left join userproject where ismanager) as projectdata natural join users;
 
 -- Features
 select featurename, progress, starttime, endtime, currentrisk from features natural join userproject where userid = <userid here> and projectid = <projectid here>;
 
 -- Total tasks
-select count(*) from (select * from tasks inner join features on featureid where projectid = <projectid here>);
+select count(*) from (select * from tasks inner join features on tasks.featureid = features.featureid where projectid = <projectid here>) as totaltasks;
 
 -- Tasks to be completed
-select count(*) from (select * from tasks inner join features on featureid where projectid = <projectid here> and not completed);
+select count(*) from (select * from tasks inner join features on tasks.featureid = features.featureid where projectid = <projectid here> and not completed) as tasksleft;
