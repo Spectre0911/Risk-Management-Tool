802c3acfac4684b705424e5311268a260d0a2e5e
sanjula hettiarachchige <sanjulahetts@outlook.com>
2023-03-03 16:51:49 +0000

    labels added to feedback form slider

--- server/db.cjs
+++ server/db.cjs
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "Avazbek2002",
+  password: "root",
   host: "localhost",
   port: 5432,
   database: "riskmanager"
