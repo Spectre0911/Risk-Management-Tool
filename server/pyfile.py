72d507b24ac775f3610f8e39c54902aed7a002bc
Avazbek Isroilov <64166521+Avazbek2002@users.noreply.github.com>
2023-03-12 22:07:37 +0000

    Update pyfile.py

--- server/pyfile.py
+++ server/pyfile.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
 
 # Setup url route which will calculate
-# total sum of array.
+# the probabilty of success.
 @app.route('/predictoveralscore', methods = ['POST']) 
 def predict():
     #recieve the post request
@@ -30,14 +30,13 @@ def predict():
     projectid = int(data['projectid'])
 
     #get the necessary features
-
     skillset_per_workload = skillset_workload(projectid)
     delay = get_delay(projectid)
     bugs = get_bugs(projectid)
     replacement_score = get_replacement(projectid)
     change_features = get_change_features(projectid)
     feedback = get_feedback(projectid)
-    success_story = 0.5
+    success_story = 0.5 #for now since there is no any history of success of a company
 
     #merge all the features into one numpy array
     in_data = np.array([skillset_per_workload, success_story, delay, bugs, feedback, change_features, replacement_score]).reshape(1,-1)
@@ -100,50 +99,57 @@ def get_delay (projectId):
 
     return delay/feature_size
 
-
+#calculate the code quality score
 def get_bugs (projectId):
     cursorObj = conn.cursor()
-
+    
+    # retrieve the sum of severity of all bugs in the project
     cursorObj.execute("SELECT SUM(severity) FROM features NATURAL JOIN bugs WHERE projectid = %s", (projectId,))
     num_bugs = cursorObj.fetchall()[0][0]
 
     if num_bugs == None:
         return 0
-
+    
+    #count the number of features of that project
     cursorObj.execute("SELECT COUNT(*) FROM features WHERE projectid = %s", (projectId,))
     feature_size = cursorObj.fetchall()[0][0] * 1.5
 
     return num_bugs/feature_size
 
+#get the team-member replacement score
 def get_replacement (projectId):
     cursorObj = conn.cursor()
-
+    
+    #get the dates when the team members were removed from the project
     cursorObj.execute("SELECT dateChanged FROM replacements WHERE replacements.changeType = %s", (0,))
     dates_changed = cursorObj.fetchall()
     if len(dates_changed) == 0:
         return 0
 
     dates_changed = [item[0] for item in dates_changed]
-
+    
+    #fetch the started date and the deadline of a project
     cursorObj.execute("SELECT opened, deadline FROM projects WHERE projectid = %s", (projectId,))
     (opened, deadline) = cursorObj.fetchall()[0]
     duration = (deadline - opened).days
     replacement_score = 0
-
+    
+    #count the number of users working on a project
     cursorObj.execute("SELECT count(*) FROM userproject WHERE projectid = %s", (projectId,))
     num_employees = cursorObj.fetchall()[0][0]/8
-
+    
+    #select the replacements which happened after 0.1*duration days the project started
     for change in dates_changed:
         if (change - opened).days/duration > 0.1:
             replacement_score += (change - opened).days/duration
     
     return replacement_score/num_employees
 
-
+#calculate change is features score
 def get_change_features (projectId):
     cursorObj = conn.cursor()
     
-
+    #get the sum of priorities of all features that have been added to the project
     cursorObj.execute("SELECT SUM(priority) FROM (featureChange NATURAL JOIN projects) AS newTable WHERE newTable.projectid = %s and EXTRACT(DAYS from (newTable.dateChanged - newTable.opened))::INTEGER > %s", (projectId, 10))
     change_score = cursorObj.fetchall()[0][0]
     if change_score == None:
@@ -151,10 +157,11 @@ def get_change_features (projectId):
 
     return change_score/50
 
-
+#get the feedback score
 def get_feedback(projectId):
     cursorObj = conn.cursor()
     
+    #get the average of all the scores in feedback given by the developers
     cursorObj.execute("SELECT AVG(fbscore) FROM feedback WHERE projectid = %s", (projectId,))
     result = cursorObj.fetchall()[0][0]
     if result == None:
